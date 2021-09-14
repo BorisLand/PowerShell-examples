fc6f4a7df045ba4fb6cd703744adddb782d5f81d
Boris Land <38751154+BorisLand@users.noreply.github.com>
2021-09-14 07:58:20 +0200

    Update Find Site Column on ContentTypes.ps1

--- Find Site Column on ContentTypes.ps1
+++ Find Site Column on ContentTypes.ps1
@@ -1,4 +1,6 @@
-Connect-PnPOnline -Url <URL>
+# find a side column that is used in a content type
+
+Connect-PnPOnline -Url <URL> -UseWebLogin
 
 $fieldName = "<fieldName>"
 $web = Get-PnPWeb -Includes Fields,AvailableContentTypes
