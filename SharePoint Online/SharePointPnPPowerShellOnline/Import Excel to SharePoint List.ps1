c68ef0129ce8f167584827a2b2bf5d11edc67ae1
Boris Land <38751154+BorisLand@users.noreply.github.com>
2021-10-27 07:50:41 +0200

    Update Import Excel to SharePoint List.ps1

--- SharePoint Online/SharePointPnPPowerShellOnline/Import Excel to SharePoint List.ps1
+++ SharePoint Online/SharePointPnPPowerShellOnline/Import Excel to SharePoint List.ps1
@@ -1,24 +1,25 @@
+# using SharePointPnPPowerShellOnline 3.29.2101.0
 # destination credentials
 $userNameDestination = "<user account>"
 $userPasswordDestination = "<password>"
 $credDestination = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $userNameDestination, $(convertto-securestring $userPasswordDestination -asplaintext -force)
-​
+
 $url = "<site collection url>"
-​
+
 $CurrentDir = $(Get-Location).Path
 $FileName = '<excel file>.xlsx'
-​
+
 $objExcel=New-Object -ComObject Excel.Application
 $objExcel.Visible=$false
 $WorkBook=$objExcel.Workbooks.Open(('{0}\{1}' -f $CurrentDir,$FileName ))
-​
+
 $worksheet = $WorkBook.sheets.Item(1)
-​
+
 # loop for each row of the excel file
 $intRowMax = ($worksheet.UsedRange.Rows).count
-​
+
 Connect-PnPOnline -Url $url -Credentials $credDestination
-​
+
 #$intRowMax = 3 # for testing
 for($intRow = 2 ; $intRow -le $intRowMax ; $intRow++)
 {
@@ -31,10 +32,10 @@ for($intRow = 2 ; $intRow -le $intRowMax ; $intRow++)
         #"value3"= $worksheet.cells.item($intRow,4).value2;
     }    
     Write-Host "." -NoNewline
-​
+
     $item = Add-PnPListItem -List "<sharepoint list title>" -Values $values    
 }
 Disconnect-PnPOnline
-​
+
 $WorkBook.close()
 $objexcel.quit()
