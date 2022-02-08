e5fc8fec2a51d44e278ebbdfe117e53f7131fca6
Boris Land <boris.land@campana-schott.com>
2022-02-08 17:20:43 +0100

    add

---
+++ CleanProjects.ps1
@@ -0,0 +1,17 @@
+﻿cd C:\DevGit
+
+$allDirTemp = @(Get-ChildItem -Directory -Filter 'temp' -Recurse)
+foreach($folder in @($allDirTemp))
+{
+    # $folder = @($allDirTemp)[0]
+    Write-Host "Remove $($folder.FullName)"
+    Remove-Item -Path $folder.FullName -Force -Confirm:$false -Recurse
+}
+
+$allDirNodeModules = @(Get-ChildItem -Directory -Filter 'node_modules' -Recurse)
+foreach($folder in @($allDirTemp))
+{
+    # $folder = @($allDirTemp)[0]
+    Write-Host "Remove $($folder.FullName)"
+    Remove-Item -Path $folder.FullName -Force -Confirm:$false -Recurse
+}
