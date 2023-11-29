2e8eb6438cedf68f70953d5f4cb757a22d91eaa9
Boris Land <boris.landcampana-schott.com>
2023-11-29 14:25:14 +0100

    checkin

---
+++ Git check need commit.ps1
@@ -0,0 +1,23 @@
+﻿# go to your local git folder
+$rootPath = "C:\DevGit"
+cd $rootPath
+
+$allDir = @(Get-ChildItem -Directory)
+foreach($companyFolder in @($allDir))
+{
+    # $companyFolder = @($allDir)[0]
+    cd $($companyFolder.Name)
+    $allGitDirs = @(Get-ChildItem -Directory)
+    foreach($allGitDir in @($allGitDirs))
+    {
+        # $allGitDir = @($allGitDirs)[0]
+        cd $($allGitDir.Name)
+        $needCommit = git status --porcelain=v1
+        if($needCommit -ne $null)
+        {
+            Write-Host $allGitDir.FullName
+        }
+        cd ..
+    }
+    cd ..
+}
