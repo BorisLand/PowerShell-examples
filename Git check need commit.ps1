e1580b396f35edd043f44b01238316c6d229880c
Boris Land <boris.landcampana-schott.com>
2025-02-27 15:49:41 +0100

    big fix

--- Git check need commit.ps1
+++ Git check need commit.ps1
@@ -6,6 +6,7 @@ $allDir = @(Get-ChildItem -Directory)
 foreach($companyFolder in @($allDir))
 {
     # $companyFolder = @($allDir)[0]
+    Write-Host $companyFolder.Name
     cd $($companyFolder.Name)
     $allGitDirs = @(Get-ChildItem -Directory)
     foreach($allGitDir in @($allGitDirs))
